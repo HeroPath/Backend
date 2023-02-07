3593e0845a9c22e9d63baf978a25454386f2f9ee
Giancarlo <g.galvarini@alumno.um.edu.ar>
2023-02-07 11:03:30 -0300

    update userRepository

--- src/main/java/com/gianca1994/aowebbackend/resources/user/UserRepository.java
+++ src/main/java/com/gianca1994/aowebbackend/resources/user/UserRepository.java
@@ -20,25 +20,34 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
 
+    //////////////////// USED FOR AUTH_SERVICE ////////////////////
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
+    ///////////////////////////////////////////////////////////////
 
-    //getUserForGuild
+    //////////////////// USED FOR GUILD_SERVICE ////////////////////
+    @Query("SELECT u.diamond FROM User u WHERE u.id = :id")
+    int findDiamondByUserId(@Param("id") Long id);
+
+    @Modifying
+    @Query("UPDATE User u SET u.diamond = :diamond WHERE u.id = :id")
+    void updateUserDiamond(@Param("diamond") int diamond, @Param("id") Long id);
+
+    ////////////////////////////////////////////////////////////////
+
+    //////////////////// USED FOR USER_SERVICE ////////////////////
+    Page<User> findAllByOrderByLevelDescTitlePointsDescExperienceDesc(PageRequest of);
+
     @Query("SELECT new com.gianca1994.aowebbackend.resources.user.dto.response.UserGuildDTO(u.username, u.level, u.titlePoints, u.aClass, u.titleName) "
             + "FROM User u WHERE u.id = :id")
     UserGuildDTO getUserForGuild(@Param("id") Long id);
 
-    //getRankingAll
-    Page<User> findAllByOrderByLevelDescTitlePointsDescExperienceDesc(PageRequest of);
-
-    //setFreeSkillPoint
     @Query("SELECT new com.gianca1994.aowebbackend.resources.user.dto.queyModel.UserAttributes(u.aClass, u.strength, u.dexterity, u.vitality, u.intelligence, u.luck, u.freeSkillPoints, u.maxDmg, u.minDmg, u.maxHp, u.hp, u.defense, u.evasion, u.criticalChance) " +
             "FROM User u WHERE u.id = :id")
     UserAttributes findAttributesByUserId(@Param("id") Long id);
 
-    //setFreeSkillPoint
     @Modifying
     @Query("UPDATE User u SET u.strength = :strength, u.dexterity = :dexterity, u.vitality = :vitality, u.intelligence = :intelligence, u.luck = :luck, u.freeSkillPoints = :freeSkillPoints, u.maxDmg = :maxDmg, u.minDmg = :minDmg, u.maxHp = :maxHp, u.hp = :hp, u.defense = :defense, u.evasion = :evasion, u.criticalChance = :criticalChance WHERE u.id = :id")
     void updateUserStats(@Param("strength") int strength, @Param("dexterity") int dexterity, @Param("vitality") int vitality, @Param("intelligence") int intelligence, @Param("luck") int luck, @Param("freeSkillPoints") int freeSkillPoints, @Param("maxDmg") int maxDmg, @Param("minDmg") int minDmg, @Param("maxHp") int maxHp, @Param("hp") int hp, @Param("defense") int defense, @Param("evasion") int evasion, @Param("criticalChance") float criticalChance, @Param("id") Long id);
-
+    ////////////////////////////////////////////////////////////////
 }
