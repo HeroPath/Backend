05310535a256c29d8065a11d92e77dbe2086a4da
Giancarlo <g.galvarini@alumno.um.edu.ar>
2023-02-13 18:36:26 -0300

    update

--- src/main/java/com/gianca1994/aowebbackend/resources/mail/MailController.java
+++ src/main/java/com/gianca1994/aowebbackend/resources/mail/MailController.java
@@ -1,11 +1,10 @@
 package com.gianca1994.aowebbackend.resources.mail;
 
+import com.gianca1994.aowebbackend.resources.jwt.config.JwtTokenUtil;
+import com.gianca1994.aowebbackend.resources.mail.dto.request.SendMailDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,9 +16,22 @@ public class MailController {
     @Autowired
     private MailService mailS;
 
+    @Autowired
+    private JwtTokenUtil jwt;
+
     @GetMapping
     @PreAuthorize("hasAuthority('ADMIN')")
     public List<Mail> getAllMails() {
         return mailS.getMails();
+    }
+
+    @PostMapping("/send")
+    @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('STANDARD')")
+    public void sendMail(@RequestHeader(value = "Authorization") String token,
+                         @RequestBody SendMailDTO mail) {
+        mailS.sendMail(
+                jwt.getUsernameFromToken(token.substring(7)),
+                mail
+        );
     }
 }
