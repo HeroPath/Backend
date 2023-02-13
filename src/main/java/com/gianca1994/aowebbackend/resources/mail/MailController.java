74cd86fba23370406d7675181342ca724a929f5a
Giancarlo <g.galvarini@alumno.um.edu.ar>
2023-02-13 19:10:07 -0300

    update

--- src/main/java/com/gianca1994/aowebbackend/resources/mail/MailController.java
+++ src/main/java/com/gianca1994/aowebbackend/resources/mail/MailController.java
@@ -1,5 +1,6 @@
 package com.gianca1994.aowebbackend.resources.mail;
 
+import com.gianca1994.aowebbackend.exception.Conflict;
 import com.gianca1994.aowebbackend.resources.jwt.config.JwtTokenUtil;
 import com.gianca1994.aowebbackend.resources.mail.dto.request.SendMailDTO;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,13 +23,26 @@ public class MailController {
     @GetMapping
     @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('STANDARD')")
     public List<Mail> getAllMails(@RequestHeader(value = "Authorization") String token) {
+        /**
+         * @Author: Gianca1994
+         * Explanation: This method is used to get all the mails of the user
+         * @param String token
+         * @return List<Mail>
+         */
         return mailS.getMails(jwt.getUsernameFromToken(token.substring(7)));
     }
 
     @PostMapping("/send")
     @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('STANDARD')")
     public void sendMail(@RequestHeader(value = "Authorization") String token,
-                         @RequestBody SendMailDTO mail) {
+                         @RequestBody SendMailDTO mail) throws Conflict {
+        /**
+         * @Author: Gianca1994
+         * Explanation: This method is used to send a mail to another user
+         * @param String token
+         * @param SendMailDTO mail
+         * @return void
+         */
         mailS.sendMail(
                 jwt.getUsernameFromToken(token.substring(7)),
                 mail
