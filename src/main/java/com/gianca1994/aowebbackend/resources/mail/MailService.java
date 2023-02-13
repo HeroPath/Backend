df9926a7df9e99923c9f0a51847aec1f57729dfe
Giancarlo <g.galvarini@alumno.um.edu.ar>
2023-02-13 18:24:27 -0300

    mail service

---
+++ src/main/java/com/gianca1994/aowebbackend/resources/mail/MailService.java
@@ -0,0 +1,17 @@
+package com.gianca1994.aowebbackend.resources.mail;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MailService {
+
+    @Autowired
+    private MailRepository mailR;
+
+    public List<Mail> getMails() {
+        return mailR.findAll();
+    }
+}
