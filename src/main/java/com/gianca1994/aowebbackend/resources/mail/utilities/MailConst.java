a9b1c4bd7bef0aefdff142b8425b1b6c3bb55644
Giancarlo <g.galvarini@alumno.um.edu.ar>
2023-02-13 19:07:56 -0300

    update

---
+++ src/main/java/com/gianca1994/aowebbackend/resources/mail/utilities/MailConst.java
@@ -0,0 +1,8 @@
+package com.gianca1994.aowebbackend.resources.mail.utilities;
+
+public class MailConst {
+    public static final String USER_NOT_FOUND = "User not found";
+    public static final String RECEIVER_EMPTY = "Receiver can't be empty";
+    public static final String SUBJECT_EMPTY = "Subject can't be empty";
+    public static final String MESSAGE_EMPTY = "Message can't be empty";
+}
--- src/main/java/com/gianca1994/aowebbackend/resources/mail/utilities/MailServiceValidator.java
+++ src/main/java/com/gianca1994/aowebbackend/resources/mail/utilities/MailServiceValidator.java
@@ -5,18 +5,18 @@ import com.gianca1994.aowebbackend.exception.Conflict;
 public class MailServiceValidator {
 
     public void userExist(boolean exist) throws Conflict {
-        if (!exist) throw new Conflict("User not found");
+        if (!exist) throw new Conflict(MailConst.USER_NOT_FOUND);
     }
 
     public void receiverNotEmpty(String receiver) throws Conflict {
-        if (receiver.isEmpty()) throw new Conflict("Receiver can't be empty");
+        if (receiver.isEmpty()) throw new Conflict(MailConst.RECEIVER_EMPTY);
     }
 
     public void subjectNotEmpty(String subject) throws Conflict {
-        if (subject.isEmpty()) throw new Conflict("Subject can't be empty");
+        if (subject.isEmpty()) throw new Conflict(MailConst.SUBJECT_EMPTY);
     }
 
     public void messageNotEmpty(String message) throws Conflict {
-        if (message.isEmpty()) throw new Conflict("Message can't be empty");
+        if (message.isEmpty()) throw new Conflict(MailConst.MESSAGE_EMPTY);
     }
 }
