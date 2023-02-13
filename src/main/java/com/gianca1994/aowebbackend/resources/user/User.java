744171d67a92b5ff9557987027449ae04d9d6494
Giancarlo <g.galvarini@alumno.um.edu.ar>
2023-02-13 18:52:32 -0300

    update

--- src/main/java/com/gianca1994/aowebbackend/resources/user/User.java
+++ src/main/java/com/gianca1994/aowebbackend/resources/user/User.java
@@ -77,6 +77,7 @@ public class User {
                     referencedColumnName = "id"),
             inverseJoinColumns = @JoinColumn(name = "mail_id",
                     referencedColumnName = "id"))
+    @JsonIgnore
     private Set<Mail> mail;
 
     @Column
