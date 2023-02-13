1e16ed2164f363111f5b696054068d336695da65
Giancarlo <g.galvarini@alumno.um.edu.ar>
2023-02-13 19:09:30 -0300

    update

--- src/main/java/com/gianca1994/aowebbackend/resources/mail/utilities/MailServiceValidator.java
+++ src/main/java/com/gianca1994/aowebbackend/resources/mail/utilities/MailServiceValidator.java
@@ -5,18 +5,42 @@ import com.gianca1994.aowebbackend.exception.Conflict;
 public class MailServiceValidator {
 
     public void userExist(boolean exist) throws Conflict {
+        /**
+         * @Author: Gianca1994
+         * Explanation: This method checks if the user exists
+         * @param boolean exist
+         * @return void
+         */
         if (!exist) throw new Conflict(MailConst.USER_NOT_FOUND);
     }
 
     public void receiverNotEmpty(String receiver) throws Conflict {
+        /**
+         * @Author: Gianca1994
+         * Explanation: This method checks if the receiver is empty
+         * @param String receiver
+         * @return void
+         */
         if (receiver.isEmpty()) throw new Conflict(MailConst.RECEIVER_EMPTY);
     }
 
     public void subjectNotEmpty(String subject) throws Conflict {
+        /**
+         * @Author: Gianca1994
+         * Explanation: This method checks if the subject is empty
+         * @param String subject
+         * @return void
+         */
         if (subject.isEmpty()) throw new Conflict(MailConst.SUBJECT_EMPTY);
     }
 
     public void messageNotEmpty(String message) throws Conflict {
+        /**
+         * @Author: Gianca1994
+         * Explanation: This method checks if the message is empty
+         * @param String message
+         * @return void
+         */
         if (message.isEmpty()) throw new Conflict(MailConst.MESSAGE_EMPTY);
     }
 }
