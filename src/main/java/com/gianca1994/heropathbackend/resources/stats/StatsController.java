c498f58ab747a5dd4f987f1972ab7c7cde562132
Giancarlo <g.galvarini@alumno.um.edu.ar>
2023-03-15 16:52:11 -0300

    update

--- src/main/java/com/gianca1994/heropathbackend/resources/stats/StatsController.java
+++ src/main/java/com/gianca1994/heropathbackend/resources/stats/StatsController.java
@@ -1,6 +1,8 @@
 package com.gianca1994.heropathbackend.resources.stats;
 
 import com.gianca1994.heropathbackend.config.SvConfig;
+import com.gianca1994.heropathbackend.resources.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/stats")
 public class StatsController {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping()
     public StatsModel getStats() {
         return new StatsModel(
+                userRepository.countUsers(),
                 SvConfig.LEVEL_MAX,
                 SvConfig.EXPERIENCE_MULTIPLIER,
                 SvConfig.GOLD_MULTIPLIER
--- src/main/java/com/gianca1994/heropathbackend/resources/stats/StatsModel.java
+++ src/main/java/com/gianca1994/heropathbackend/resources/stats/StatsModel.java
@@ -3,10 +3,13 @@ package com.gianca1994.heropathbackend.resources.stats;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 @AllArgsConstructor
 @Getter
+@Setter
 public class StatsModel {
+    private int userRegistered;
     private int lvlMax;
     private int expMultiplier;
     private int goldMultiplier;
