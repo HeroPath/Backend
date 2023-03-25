c2a893a5d821a8333ba5b22f19dd9e5f1b177b60
Giancarlo <g.galvarini@alumno.um.edu.ar>
2023-03-25 13:41:20 -0300

    update

--- src/main/java/com/gianca1994/heropathbackend/resources/market/Market.java
+++ src/main/java/com/gianca1994/heropathbackend/resources/market/Market.java
@@ -1,13 +1,17 @@
 package com.gianca1994.heropathbackend.resources.market;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.gianca1994.heropathbackend.resources.inventory.Inventory;
 import com.gianca1994.heropathbackend.resources.item.Item;
+import com.gianca1994.heropathbackend.resources.market.marketRelations.MarketItem;
+import com.gianca1994.heropathbackend.resources.user.userRelations.userMail.UserMail;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 @Table(name = "market")
@@ -29,5 +33,9 @@ public class Market {
 
     @Column
     private String usernameSeller;
+
+    @OneToMany(mappedBy = "market", fetch = FetchType.EAGER, cascade = CascadeType.REMOVE)
+    @JsonIgnore
+    private MarketItem marketItem;
 
 }
