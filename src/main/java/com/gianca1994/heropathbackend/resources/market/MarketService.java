d6d748e31dc6e0e378aff8213f64c4d52d4e9839
Giancarlo <g.galvarini@alumno.um.edu.ar>
2023-03-25 15:35:04 -0300

    update

---
+++ src/main/java/com/gianca1994/heropathbackend/resources/market/MarketService.java
@@ -0,0 +1,17 @@
+package com.gianca1994.heropathbackend.resources.market;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MarketService {
+
+    @Autowired
+    private MarketRepository marketRepository;
+
+    public List<Market> getAllMarkets() {
+        return marketRepository.findAll();
+    }
+}
