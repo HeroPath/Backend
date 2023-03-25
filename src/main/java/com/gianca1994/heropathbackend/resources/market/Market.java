e2ea666e15a021f88caf1e5a6bc6bbe9876c84fa
Giancarlo <g.galvarini@alumno.um.edu.ar>
2023-03-25 16:26:59 -0300

    update

--- src/main/java/com/gianca1994/heropathbackend/resources/market/Market.java
+++ src/main/java/com/gianca1994/heropathbackend/resources/market/Market.java
@@ -37,5 +37,6 @@ public class Market {
     private Item item;
 
     @Column
+    @JsonIgnore
     private Long userId;
 }
--- src/main/java/com/gianca1994/heropathbackend/resources/market/MarketService.java
+++ src/main/java/com/gianca1994/heropathbackend/resources/market/MarketService.java
@@ -1,5 +1,7 @@
 package com.gianca1994.heropathbackend.resources.market;
 
+import com.gianca1994.heropathbackend.exception.BadRequest;
+import com.gianca1994.heropathbackend.resources.item.Item;
 import com.gianca1994.heropathbackend.resources.item.ItemRepository;
 import com.gianca1994.heropathbackend.resources.market.dto.request.MarketRegisterDTO;
 import com.gianca1994.heropathbackend.resources.user.UserRepository;
@@ -7,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class MarketService {
@@ -23,11 +26,21 @@ public class MarketService {
 
     public void registerItem(Long userId, String usernameSeller, MarketRegisterDTO marketRegisterDTO) {
         Market market = new Market();
+
+        if (!itemR.existsById(marketRegisterDTO.getItemId())) throw new BadRequest("Item not found");
+
+        Item item = itemR.findById(marketRegisterDTO.getItemId()).get();
+        if (item.isInMarket()) throw new BadRequest("Item already in market");
+        if (!Objects.equals(item.getUser().getId(), userId)) throw new BadRequest("You don't own this item");
+        item.setInMarket(true);
+
         market.setUserId(userId);
-        market.setItem(itemR.findById(marketRegisterDTO.getItemId()).get());
+        market.setItem(item);
         market.setUsernameSeller(usernameSeller);
         market.setGoldPrice(marketRegisterDTO.getGoldPrice());
         market.setDiamondPrice(marketRegisterDTO.getDiamondPrice());
+
+        itemR.save(item);
         marketR.save(market);
     }
 }
