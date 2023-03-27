b1cbe312ddb6c253e15bf0b8a6c0a4d1fb92ece0
Giancarlo <g.galvarini@alumno.um.edu.ar>
2023-03-27 02:25:44 -0300

    update market

---
+++ src/main/java/com/gianca1994/heropathbackend/resources/market/utilities/MarketConst.java
@@ -0,0 +1,15 @@
+package com.gianca1994.heropathbackend.resources.market.utilities;
+
+public class MarketConst {
+    public static final String ITEM_NOT_FOUND = "Item not found";
+    public static final String MAXIMUM_ITEMS_PUBLISHED = "Maximum items published";
+    public static final String MAXIMUM_GOLD_PRICE = "Maximum gold price";
+    public static final String MAXIMUM_DIAMOND_PRICE = "Maximum diamond price";
+    public static final String ITEM_ALREADY_IN_MARKET = "Item already in market";
+    public static final String YOU_DONT_OWN_THIS_ITEM = "You don't own this item";
+    public static final String INVENTORY_FULL = "Inventory full";
+    public static final String SELLER_NOT_FOUND = "Seller not found";
+    public static final String USER_NOT_FOUND = "User not found";
+    public static final String YOU_DONT_HAVE_ENOUGH_GOLD = "You don't have enough gold";
+    public static final String YOU_DONT_HAVE_ENOUGH_DIAMOND = "You don't have enough diamond";
+}
