5d35955b5c5494f510edc3689798a43395e8a9d7
Giancarlo <g.galvarini@alumno.um.edu.ar>
2023-03-29 13:35:09 -0300

    Refactor

---
+++ src/main/java/com/gianca1994/heropathbackend/utils/Validator.java
@@ -0,0 +1,85 @@
+package com.gianca1994.heropathbackend.utils;
+
+import com.gianca1994.heropathbackend.combatSystem.GenericFunctions;
+import com.gianca1994.heropathbackend.config.SvConfig;
+import com.gianca1994.heropathbackend.exception.BadReq;
+import com.gianca1994.heropathbackend.exception.Conflict;
+import com.gianca1994.heropathbackend.exception.NotFound;
+import com.gianca1994.heropathbackend.resources.equipment.Equipment;
+import com.gianca1994.heropathbackend.resources.user.User;
+import com.gianca1994.heropathbackend.resources.user.dto.queyModel.UserAttributes;
+import com.gianca1994.heropathbackend.resources.user.dto.response.UserGuildDTO;
+
+public class Validator {
+    GenericFunctions genericFunctions = new GenericFunctions();
+
+    public void userExist(boolean exist) throws Conflict {
+        if (!exist) throw new Conflict(Const.USER.NOT_FOUND.getMsg());
+    }
+
+    public void npcExist(boolean exist) throws Conflict {
+        if (!exist) throw new Conflict(Const.NPC.NOT_FOUND.getMsg());
+    }
+
+    public void guildExist(boolean exist) throws Conflict {
+        if (!exist) throw new Conflict(Const.GUILD.NOT_FOUND.getMsg());
+    }
+
+    public void getUserForGuild(UserGuildDTO userGuildDTO) throws NotFound {
+        if (userGuildDTO == null) throw new NotFound(Const.USER.NOT_FOUND.getMsg());
+    }
+
+    public void setFreeSkillPoint(UserAttributes uAttr, String skillName) throws Conflict {
+        if (uAttr == null) throw new NotFound(Const.USER.NOT_FOUND.getMsg());
+        if (uAttr.getFreeSkillPoints() <= 0) throw new Conflict(Const.USER.DONT_HAVE_SKILLPTS.getMsg());
+        if (!Const.USER.SKILLS_ENABLED.getSkills().contains(skillName.toLowerCase()))
+            throw new Conflict(Const.USER.ALLOWED_SKILLPTS.getMsg() + Const.USER.SKILLS_ENABLED.getSkills());
+    }
+
+    public void checkAutoAttack(User attacker, User defender) throws Conflict {
+        if (attacker == defender) throw new Conflict(Const.USER.CANT_ATTACK_YOURSELF.getMsg());
+    }
+
+    public void checkDifferenceLevelPVP(short attackerLvl, short defenderLvl) throws Conflict {
+        if (attackerLvl - defenderLvl > SvConfig.MAX_LEVEL_DIFFERENCE)
+            throw new Conflict(Const.USER.CANT_ATTACK_LVL_LOWER_5.getMsg());
+    }
+
+    public void checkDifferenceLevelPVE(short userLvl, short npcLvl) throws Conflict {
+        if (npcLvl > userLvl + SvConfig.MAX_LEVEL_DIFFERENCE)
+            throw new Conflict(Const.USER.CANT_ATTACK_NPC_LVL_HIGHER_5.getMsg());
+    }
+
+    public void checkDefenderNotAdmin(User defender) throws Conflict {
+        if (defender.getRole().equals(SvConfig.ADMIN_ROLE)) throw new Conflict(Const.USER.CANT_ATTACK_ADMIN.getMsg());
+    }
+
+    public void checkLifeStartCombat(User user) {
+        if (genericFunctions.checkLifeStartCombat(user))
+            throw new BadReq(Const.USER.IMPOSSIBLE_ATTACK_LESS_HP.getMsg());
+
+    }
+
+    public void checkUserItemReqZoneSea(Equipment userEquip, String npcZone) throws Conflict {
+        if (userEquip.getItems().stream().noneMatch(item -> item.getType().equals("ship")) && npcZone.equals("sea"))
+            throw new Conflict(Const.USER.CANT_ATTACK_NPC_SEA.getMsg());
+    }
+
+    public void checkUserItemReqZoneHell(Equipment userEquip, String npcZone) throws Conflict {
+        if (userEquip.getItems().stream().noneMatch(item -> item.getType().equals("wings")) && npcZone.equals("hell"))
+            throw new Conflict(Const.USER.CANT_ATTACK_NPC_HELL.getMsg());
+    }
+
+    public void checkPvePtsEnough(User user) throws Conflict {
+        if (user.getPvePts() <= 0) throw new Conflict(Const.USER.DONT_HAVE_PVE_PTS.getMsg());
+    }
+
+    public void checkPvpPtsEnough(User user) throws Conflict {
+        if (user.getPvpPts() <= 0) throw new Conflict(Const.USER.DONT_HAVE_PVP_PTS.getMsg());
+    }
+
+    public void checkAttackerAndDefenderInSameGuild(User attacker, User defender) throws Conflict {
+        if (attacker.getGuildName().equals(defender.getGuildName()))
+            throw new Conflict(Const.USER.CANT_ATTACK_GUILD_MEMBER.getMsg());
+    }
+}
