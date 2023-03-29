eaaf9b5ea7c1d4fc403901b176a44d2009728032
Giancarlo <g.galvarini@alumno.um.edu.ar>
2023-03-29 19:00:55 -0300

    Refactor

--- src/main/java/com/gianca1994/heropathbackend/resources/npc/utilities/NpcServiceValidator.java
+++ src/main/java/com/gianca1994/heropathbackend/resources/npc/utilities/NpcServiceValidator.java
@@ -13,32 +13,14 @@ import com.gianca1994.heropathbackend.utils.Const;
 public class NpcServiceValidator {
 
     public void npcFound(boolean npcExist) {
-        /**
-         * @Author: Gianca1994
-         * @Explanation: This method is used to validate the npc before saving it
-         * @param boolean npcExist
-         * @return void
-         */
         if (!npcExist) throw new NotFound(Const.NPC.NOT_FOUND.getMsg());
     }
 
     public void npcNotFoundZone(int npcSize) {
-        /**
-         * @Author: Gianca1994
-         * @Explanation: This method is used to validate the npc before saving it
-         * @param int npcSize
-         * @return void
-         */
         if (npcSize <= 0) throw new NotFound(Const.NPC.NOT_IN_ZONE.getMsg());
     }
 
     public void saveNpc(NpcDTO npc) throws Conflict {
-        /**
-         * @Author: Gianca1994
-         * @Explanation: This method is used to validate the npc before saving it
-         * @param NpcDTO npc
-         * @return void
-         */
         if (npc.getName().isEmpty()) throw new Conflict(Const.NPC.NAME_EMPTY.getMsg());
         if (npc.getLevel() < 1) throw new Conflict(Const.NPC.LVL_LT_1.getMsg());
         if (npc.getGiveMinExp() < 0) throw new Conflict(Const.NPC.GIVE_MIN_EXP_LT_0.getMsg());
