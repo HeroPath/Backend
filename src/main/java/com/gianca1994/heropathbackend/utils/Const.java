b7f1ec59fdb57bd3c2635451b14d483b71d4ac09
Giancarlo <g.galvarini@alumno.um.edu.ar>
2023-03-29 11:40:16 -0300

    User Const - Refactor

--- src/main/java/com/gianca1994/heropathbackend/utils/Const.java
+++ src/main/java/com/gianca1994/heropathbackend/utils/Const.java
@@ -11,15 +11,15 @@ public class Const {
 
     @Getter
     public enum USER {
-        USER_NOT_FOUND("User not found"),
-        DONT_HAVE_SKILL_POINTS("You don't have any free skill points"),
+        // TODO: Continue with the user constants.
+        NOT_FOUND("User not found"),
+        DONT_HAVE_SKILLPTS("You don't have any free skill points"),
         SKILLS_ENABLED(Arrays.asList("strength", "dexterity", "intelligence", "vitality", "luck")),
         SKILL_POINT_NAME_MUST_ONE_FOLLOWING("Skill point name must be one of the following: "),
         IMPOSSIBLE_ATTACK_LESS_HP("Impossible to attack with less than 15% of life"),
         CANT_ATTACK_LVL_LOWER_5("You can't attack with a level lower than 5"),
         CANT_ATTACK_YOURSELF("You can't fight yourself"),
         CANT_ATTACK_ADMIN("You can't attack an admin"),
-        NPC_NOT_FOUND("Npc not found"),
         CANT_ATTACK_NPC_LVL_HIGHER_5("You can't attack an npc with level higher than 5 levels higher than you"),
         CANT_ATTACK_NPC_SEA("You can't attack an npc in the sea without a ship"),
         CANT_ATTACK_NPC_HELL("You can't attack an npc in hell without wings"),
